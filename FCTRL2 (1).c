5155fc809caba5cb61ab42579d27a0e179841e82
vISHALsINGHrATHOR <145266272+vISHALsINGHrATHOR@users.noreply.github.com>
2023-09-18 22:20:15 +0530

    Add files via upload

---
+++ FCTRL2 (1).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    float p_i=3.14,r,A_;
+    printf("Enter the Radius of Circle : ");
+    scanf("%f",&r);
+    A_=p_i*r*r;
+    printf("Area of the Circle = %f ",A_);
+}
