9f1379cb9b77ae2f3243a1640fcaa5dd133e9137
vISHALsINGHrATHOR <145266272+vISHALsINGHrATHOR@users.noreply.github.com>
2023-09-19 21:02:27 +0530

    Add files via upload

---
+++ DAY 2 .c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int L,B,A;
+    printf("Enter the Length of the Rectangle: ");
+    scanf("%d",&L);
+    printf("Enter the Breadth of the Rectangle: ");
+    scanf("%d",&B);
+    A=L*B;
+    printf("Area of the Rectangle = %d",A);
+}
