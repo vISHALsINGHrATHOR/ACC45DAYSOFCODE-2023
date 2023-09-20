3a2002a6ca08c348bba7b5b843f7ba7f98988b0a
vISHALsINGHrATHOR <145266272+vISHALsINGHrATHOR@users.noreply.github.com>
2023-09-20 15:39:41 +0530

    Delete FCTRL2 (1).c

--- FCTRL2 (1).c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    float p_i=3.14,r,A_;
-    printf("Enter the Radius of Circle : ");
-    scanf("%f",&r);
-    A_=p_i*r*r;
-    printf("Area of the Circle = %f ",A_);
-}
