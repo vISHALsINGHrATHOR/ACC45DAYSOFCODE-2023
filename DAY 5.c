c4eb148b734d378705c4fd7a21185eaf21c9fbc8
vISHALsINGHrATHOR <145266272+vISHALsINGHrATHOR@users.noreply.github.com>
2023-09-22 12:16:04 +0530

    Add files via upload

---
+++ DAY 5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if (x>15 && x<100){
+        printf("NO");}
+    else if (x<=15 && x>=1){
+        printf("YES");}
+    else{
+        printf("Invalid Battery %");
+    }
+}
