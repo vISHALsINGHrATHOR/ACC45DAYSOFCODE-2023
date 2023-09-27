021d49a07c48e691d2bea651c2da0d581a28b0e0
vISHALsINGHrATHOR <145266272+vISHALsINGHrATHOR@users.noreply.github.com>
2023-09-27 09:26:49 +0530

    Add files via upload

---
+++ DAY 10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+   int a,b,c;
+   // insertingthe amount Chef has and the cost of one chocolate respectively.
+   scanf("%d %d",&a,&b);
+   c=a/b
+   // printing the maximum number of chocolates Chef can buy
+   printf("%d",c);
+}
